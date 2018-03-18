56339eb10441af5c1bd51f9143abb36767006b15
lapinell <lapinell@gmail.com>
2018-03-18 12:27:26 -0500

    created callAPIs module and added api key and url

---
+++ js/_callAPIs.js
@@ -0,0 +1,4 @@
+"use strict";
+
+const eventbriteAPIkey = "KD72TKWVWZCHLBA5VKDB";
+const eventbriteURL = `https://www.eventbrite.com/oauth/authorize?response_type=token&client_id=${eventbriteAPIkey}`;
