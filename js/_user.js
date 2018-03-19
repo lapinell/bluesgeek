ed65124ac9aa5f0b2d1dcab8570ca30d5e52c794
lapinell <lapinell@gmail.com>
2018-03-19 10:21:07 -0500

    created currentUser variable

--- js/_user.js
+++ js/_user.js
@@ -1,1 +1,16 @@
 "use strict";
+
+let currentUser = {
+    uid: null,
+    firstName: null,
+    lastName: null,
+    email: null,
+    password: null,
+    streetAddress: null,
+    city: null,
+    state: null,
+    zipcode: null,
+    country: null,
+    community: null,
+    fbid: null,
+};
