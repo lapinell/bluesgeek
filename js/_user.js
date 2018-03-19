d870f6df43f6cd2c44b91bcd7aad353c233c78f0
lapinell <lapinell@gmail.com>
2018-03-19 10:38:08 -0500

    added listen for change state function; added makeNewUser function

--- js/_user.js
+++ js/_user.js
@@ -1,5 +1,8 @@
 "use strict";
 
+let firebase = require("./_firebase-config"),
+    fbInteraction = require("./_firebase-interaction");
+
 let currentUser = {
     uid: null,
     firstName: null,
@@ -12,5 +15,33 @@ let currentUser = {
     zipcode: null,
     country: null,
     community: null,
-    fbid: null,
+    fbid: null
+};
+
+//listen for changed state
+firebase.auth().onAuthStateChanged((user) => {
+    console.log("onAuthStateChanged", user);
+    if (user) {
+        currentUser.uid = user.uid;
+        console.log("current user:", currentUser);
+    } else {
+        console.log("user not logged in", currentUser);
+    }
+});
+
+let makeNewUser = (uid) => {
+    let userObj = {
+        uid: uid,
+        firstName: null,
+        lastName: null,
+        email: null,
+        password: null,
+        streetAddress: null,
+        city: null,
+        state: null,
+        zipcode: null,
+        country: null,
+        community: null,
+        fbid: null
+    };
 };
