088492b05709b687d635327cb58eb05f3b53ee7e
Laura Pinell <lapinell@gmail.com>
2018-03-19 21:04:50 -0500

    Update _firebase-config.js

--- js/_firebase-config.js
+++ js/_firebase-config.js
@@ -6,12 +6,7 @@ require('firebase/database');
 
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyCUPRfGGlr1N9Wq8rFfnlbxQiGPxwc9d3A",
-    authDomain: "bluesgeek-9f913.firebaseapp.com",
-    databaseURL: "https://bluesgeek-9f913.firebaseio.com",
-    projectId: "bluesgeek-9f913",
-    storageBucket: "bluesgeek-9f913.appspot.com",
-    messagingSenderId: "483861606831"
+
   };
   
   firebase.initializeApp(config);
