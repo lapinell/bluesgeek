5194b7587b62e0054e83fa6f2a4f96c7b48d6c6c
lapinell <lapinell@gmail.com>
2018-03-30 17:10:53 -0500

    able to print ticket information to the dom!

--- js/_registration.js
+++ js/_registration.js
@@ -11,31 +11,44 @@ eventbrite.GETeventDetails(bluesGeekID)
     console.log('these are the bluesGeek event details', eventDetails);
     console.log('ticket classes:', eventDetails.ticket_classes);
     let ticketClasses = eventDetails.ticket_classes;
-
-    grabEachObj(ticketClasses);
+    return grabEachObj(ticketClasses);
+})
+.then((tickets) => {
+    // console.log('got the tickets array', tickets);
+    printTickets(tickets);
 });
 
 let grabEachObj = (arrayOfObjects) => {
-    console.log('arrayOfObject function:', arrayOfObjects);
+    // console.log('arrayOfObject function:', arrayOfObjects);
     for(let i = 0; i < arrayOfObjects.length; i++) {
         let eachObject = arrayOfObjects[i];
-        console.log('eachObject', eachObject);
+        // console.log('eachObject', eachObject);
         grabDetails(eachObject);
     }
-    console.log('arrayOfObjects after:', arrayOfObjects);
+    // console.log('arrayOfObjects after:', arrayOfObjects);
     return arrayOfObjects;
 };
 
 let grabDetails = (object) => {
-    console.log("object is", object);
-    // let newTicketObj = {};
+    // console.log("object is", object);
     for (let prop in object) { //for each property in the object
         if (object[prop] == null || object[prop] == undefined) { // if the property is null or undefined
-            // console.log("obj inside if statement", object);
-            // console.log("prop inside if statement", object[prop]);
             delete object[prop]; // delete the property and its null/undefined value
         }
     }
-    console.log("object after:", object);
+    // console.log("object after:", object);
     return object;
+};
+
+let printTickets = (ticketArray) => {
+    console.log('what are tickets?', ticketArray);
+    
+    for(let i = 0; i < ticketArray.length; i++) {
+        let ticket = ticketArray[i];
+        console.log('the ticket is:' , ticket);
+        $('main').append(`
+        <section>
+            <h2>${ticket.name}</h2>
+        </section>`);
+    }
 };
